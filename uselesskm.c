16eb10e825cc00bb3f96c3d71d3999f810066e1e
Egor Dudyak <gggorec@gmail.com>
2016-12-04 08:51:45 +0500

    Добавлены функции трансфера запросов/ответов. Не тестировалось.

--- uselesskm.c
+++ uselesskm.c
@@ -14,7 +14,7 @@
 #include <linux/delay.h>
 #define KM_AUTHOR "Egor Dudyak"
 #define KM_DESC   "Provides connection between UselesDBd and UselessDBClients"
-#define LOGPREFIX	"UselessDB module: "
+#define LOG_PREFIX	"UselessDB module: "
 
 #define NETLINK_USELESS_P 		31
 #define MAX_CLIENT_COUNT		2
@@ -27,31 +27,28 @@
 
 #define ERROR_WRONG_MSG_TYPE		1
 #define ERROR_DAEMON_UNREACHABLE	2
+#define ERROR_TRANSFER_FAILED		3
 
 static int daemon_pid=0;
 struct sock *nl_sk = NULL;
-static client_struct clients[MAX_CLIENT_COUNT];
 
 static void nl_recv_msg_handler(struct sk_buff *skb);
 static int send_ack_to_daemon(int new_pid);
 static void transfer_msg_to_daemon(struct nlmsghdr *nlh);
 static void transfer_msg_to_client(struct nlmsghdr *nlh);
 static void send_error_to_src_app(int pid, int error_code);
+static void put_src_pid_to_msg(void);
+static void get_src_pid_from_msg(void);
 
 static int __init uselesskm_init(void){
 	printk(KERN_INFO "Starting uselessdb module\n");
     struct netlink_kernel_cfg cfg = {
         .input = nl_recv_msg_handler,
     };
-    int i=0;
-    for(i=0;i<MAX_CLIENT_COUNT;i++){
-    	clients[i].pid=0;
-    	clients[i].msg_cnt=0;
-    }
 
     nl_sk = netlink_kernel_create(&init_net, NETLINK_USELESS_P, &cfg);
     if (!nl_sk) {
-        printk(KERN_ALERT LOGPREFIX"Error creating socket.\n");
+        printk(KERN_ALERT LOG_PREFIX"Error creating socket.\n");
         return -10;
     }
 
@@ -79,10 +76,7 @@ static void nl_recv_msg_handler(struct sk_buff *skb){
     	case MSGTYPE_SET_DPID:
     		if(!send_ack_to_daemon(pid)){
     			daemon_pid=pid;
-    			printk(KERN_INFO LOGPREFIX"New daemon id=%d setted\n",daemon_pid);
-    		}
-    		else{
-    			printk(KERN_INFO LOGPREFIX"Error while setting new daemon id\n",daemon_pid);
+    			printk(KERN_INFO LOG_PREFIX"New daemon id=%d setted\n",daemon_pid);
     		}
     		break;
     	default:
@@ -97,37 +91,6 @@ static void nl_recv_msg_handler(struct sk_buff *skb){
     		}
     		break;
     }
-    /*
-    for(i=0;i<MAX_CLIENT_COUNT;i++){
-    	if(clients[i].pid==pid){
-    		clients[i].msg_cnt++;
-    		break;
-    	}
-    	else if(clients[i].pid==0){
-    		clients[i].pid=pid;
-    		clients[i].msg_cnt++;
-    		break;
-    	}
-    }
-    if(i<MAX_CLIENT_COUNT){
-		printk(KERN_INFO LOGPREFIX"Client_num=%d Pid=%d msg_num=%d msg_type=%d payload:%s\n",i,pid,clients[i].msg_cnt, nlh->nlmsg_type, (char *)nlmsg_data(nlh));
-
-		skb_out = nlmsg_new(msg_size, 0);
-		if (!skb_out) {
-			printk(KERN_ERR LOGPREFIX"Failed to allocate new skb\n");
-			return;
-		}
-
-		nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
-		NETLINK_CB(skb_out).dst_group = 0; // not in mcast group
-		strncpy(nlmsg_data(nlh), msg, msg_size);
-		msleep(1000);
-		res = nlmsg_unicast(nl_sk, skb_out, pid);
-		if (res < 0)
-			printk(KERN_INFO LOGPREFIX"Error while sending bak to user\n");
-
-
-	}*/
 }
 
 static int send_ack_to_daemon(int new_pid){
@@ -136,7 +99,7 @@ static int send_ack_to_daemon(int new_pid){
 	struct sk_buff *skb_out;
 	skb_out = nlmsg_new(NLMSG_DEFAULT_SIZE, 0);
 	if (!skb_out) {
-		printk(KERN_ERR LOGPREFIX"Failed to allocate new skb\n");
+		printk(KERN_ERR LOG_PREFIX"Failed to allocate new skb\n");
 		ret=1;
 	}
 	if(ret==0){
@@ -144,18 +107,64 @@ static int send_ack_to_daemon(int new_pid){
 		NETLINK_CB(skb_out).dst_group = 0; // not in mcast group
 		ret = nlmsg_unicast(nl_sk, skb_out, new_pid);
 		//nlmsg_free(skb_out);	виснет(((
+		if(ret<0){
+			printk(KERN_INFO LOG_PREFIX"Error while setting new daemon id\n",daemon_pid);
+		}
 	}
 	return ret;
 }
 
 static void transfer_msg_to_daemon(struct nlmsghdr *nlh){
+	int ret=0;
+	struct sk_buff *skb_out;
+	struct nlmsghdr *nlh_out;
+	int new_msg_size=nlmsg_len(nlh)+sizeof(nlh->nlmsg_pid);
 	if(daemon_pid!=0){
-
+		skb_out = nlmsg_new(new_msg_size, 0);
+		if (!skb_out) {
+			printk(KERN_ERR LOG_PREFIX"Failed to allocate new skb\n");
+			ret=1;
+		}
+		else{
+			nlh_out = nlmsg_put(skb_out, 0, 0, nlh->nlmsg_type, new_msg_size, 0);
+			NETLINK_CB(skb_out).dst_group = 0; // not in mcast group
+			memcpy(nlmsg_data(nlh_out),(void *)(&nlh->nlmsg_pid),sizeof(nlh->nlmsg_pid));
+			memcpy((void *)((char *)nlmsg_data(nlh_out)+sizeof(nlh->nlmsg_pid)),nlmsg_data(nlh),nlmsg_len(nlh));
+			printk("%d %s\n",*((__u32 *)nlmsg_data(nlh_out)),((char *)nlmsg_data(nlh_out)+sizeof(nlh->nlmsg_pid)));	//debug str;
+			ret = nlmsg_unicast(nl_sk, skb_out, daemon_pid);
+			if(ret<0){
+				printk(KERN_INFO LOG_PREFIX"Error while transferring msg from pid %d to daemon\n",nlh->nlmsg_pid);
+				send_error_to_src_app(nlh->nlmsg_pid,ERROR_TRANSFER_FAILED);
+			}
+		}
+	}
+	else{
+		printk(KERN_INFO LOG_PREFIX"Daemon unreachable\n");
+		send_error_to_src_app(nlh->nlmsg_pid,ERROR_DAEMON_UNREACHABLE);
 	}
 }
 
 static void transfer_msg_to_client(struct nlmsghdr *nlh){
-	//todo implement
+	int ret=0;
+	struct sk_buff *skb_out;
+	struct nlmsghdr *nlh_out;
+	int new_msg_size=nlmsg_len(nlh)-sizeof(nlh->nlmsg_pid);
+	__u32 client_pid=*((__u32 *)nlmsg_data(nlh));
+	skb_out = nlmsg_new(new_msg_size, 0);
+	if (!skb_out) {
+		printk(KERN_ERR LOG_PREFIX"Failed to allocate new skb\n");
+		ret=1;
+	}
+	else{
+		nlh_out = nlmsg_put(skb_out, 0, 0, nlh->nlmsg_type, new_msg_size, 0);
+		NETLINK_CB(skb_out).dst_group = 0; // not in mcast group
+		memcpy(nlmsg_data(nlh_out),((char *)nlmsg_data(nlh_out)+sizeof(client_pid)),new_msg_size);
+		printk("%d %s\n",client_pid,(char *)nlmsg_data(nlh_out));	//todo remove
+		ret = nlmsg_unicast(nl_sk, skb_out, client_pid);
+		if(ret<0){
+			printk(KERN_INFO LOG_PREFIX"Error while transferring msg from daemon to pid %d \n",nlh->nlmsg_pid);
+		}
+	}
 }
 
 static void send_error_to_src_app(int pid,int error_code){
